91d2dec59c5997bbd021ce1fd56bd760b3eb6ca8
sinne24 <cindy.hernandez5224@gmail.com>
2022-11-03 11:38:15 -0600

    commit try again

---
+++ src/com/games/blackjack/controller/Controller.java
@@ -0,0 +1,10 @@
+package com.games.blackjack.controller;
+
+import com.games.blackjack.Dealer;
+
+import java.util.Scanner;
+
+public class Controller {
+    private Scanner scanner = new Scanner(System.in);
+    private Dealer dealer;
+}
